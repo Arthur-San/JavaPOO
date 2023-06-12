1156b0d41e72797edf4b48c657fc3d2a1ab929a4
Arthur dos Santos <67018260+Arthur-San@users.noreply.github.com>
2023-06-12 13:10:51 -0300

    + aula de Classes_Interfaces_e_Enumeradores

---
+++ src/Classes_Interfaces_e_Enumeradores/BoloComCoberturaDeChocolate.java
@@ -0,0 +1,8 @@
+package Classes_Interfaces_e_Enumeradores;
+
+public class BoloComCoberturaDeChocolate extends Bolo {
+    @Override //opcional
+    void assarBolo() {
+        temperatura += 15;
+    }
+}
---
+++ src/Classes_Interfaces_e_Enumeradores/Main.java
@@ -0,0 +1,19 @@
+package Classes_Interfaces_e_Enumeradores;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Bolo bolo = new Bolo();
+        bolo.nome = "Meu bolo";
+
+        Bolo bolo2 = new Bolo();
+        bolo2.nome = "Meu bolo";
+
+        System.out.println(bolo.equals(bolo2) );
+        //de padrão retorna false, porque a refenencia de bolo é diferente de bolo2
+        //após sobrescrever o método equals para comparar NOME ao invés de referência, retorna true caso sejam iguais
+
+    }
+}
